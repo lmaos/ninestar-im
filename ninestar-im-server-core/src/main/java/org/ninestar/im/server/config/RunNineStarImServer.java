cc5eb6fcfb9969286074be5c5587982b85fd6104
lmaos <m.vip@qq.com>
2019-08-26 10:13:30 +0800

    增加 Spring @Conditional， 优化启动的方式

--- ninestar-im-server-core/src/main/java/org/ninestar/im/server/config/RunNineStarImServer.java
+++ ninestar-im-server-core/src/main/java/org/ninestar/im/server/config/RunNineStarImServer.java
@@ -1,18 +1,27 @@
 package org.ninestar.im.server.config;
 
 import org.ninestar.im.server.NineStarImServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportAware;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.core.type.AnnotationMetadata;
 
 @Configuration
-public class RunNineStarImServer implements InitializingBean, ImportAware {
+@Conditional(RunNineStarImServer.class)
+public class RunNineStarImServer implements InitializingBean, ImportAware, Condition {
+	
+	private static final Logger log = LoggerFactory.getLogger(RunNineStarImServer.class);
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
-
+		log.info("九星服务启动");	
 	}
 
 	private AnnotationMetadata importingClassMetadata;
@@ -26,13 +35,15 @@ public class RunNineStarImServer implements InitializingBean, ImportAware {
 		return server;
 	}
 
-	// @Bean
-	// NineStarImSerV0Handler handlerV0() {
-	// return new NineStarImSerV0Handler();
-	// }
 	@Override
 	public void setImportMetadata(AnnotationMetadata importMetadata) {
 		this.importingClassMetadata = importMetadata;
+	}
 
+	@Override
+	public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+		String[] startAnns = context.getBeanFactory().getBeanNamesForAnnotation(EnableNineStarImServer.class);
+
+		return startAnns != null && startAnns.length > 0;
 	}
 }
