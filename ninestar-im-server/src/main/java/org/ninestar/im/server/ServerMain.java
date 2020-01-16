8f3e4b9ac851ba9c791abb2946e8fc081caa5454
lmaos <m.vip@qq.com>
2020-01-16 14:08:11 +0800

    x

--- ninestar-im-server/src/main/java/org/ninestar/im/server/ServerMain.java
+++ ninestar-im-server/src/main/java/org/ninestar/im/server/ServerMain.java
@@ -16,7 +16,7 @@ public class ServerMain implements InitializingBean, ApplicationContextAware {
 
 	@Bean
 	NineStarImServer nineStarImServer() {
-		NineStarImServer nineStarImServer = new NineStarImServer(null);
+		NineStarImServer nineStarImServer = new NineStarImServer();
 		nineStarImServer.setPort(7788);
 		return nineStarImServer;
 	}
